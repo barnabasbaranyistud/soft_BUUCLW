729382ab2e3ba882a5fb914e56e23839a0c62797
barnabasbaranyistud <barnabas.baranyi2@stud.uni-corvinus.hu>
2024-02-22 11:15:34 +0100

    pascal kész

--- Pascal/Form1.cs
+++ Pascal/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Win32.SafeHandles;
+
 namespace Pascal
 {
     public partial class Form1 : Form
@@ -5,6 +7,33 @@ namespace Pascal
         public Form1()
         {
             InitializeComponent();
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop <= 10; oszlop++)
+                {
+                    Button b = new Button();
+                    b.Top = sor * 60;
+                    b.Left = ClientRectangle.Width / 2 - 60*oszlop + sor*30;
+                    b.Height = 60;
+                    b.Width = 60;
+                    int p = Faktorialis(sor) / (Faktorialis(oszlop) * (Faktorialis(sor - oszlop)));
+                    b.Text = p.ToString();
+                    if (int.Parse(b.Text) >= 1) Controls.Add(b);
+                }
+            }
+        }
+        int Faktorialis(int n)
+        {
+            int eredmény = 1;
+            for (int i = 1; i <= n; i++) eredmény *= i;
+
+            return eredmény;
+        }
+
+        int Faktorialis2(int n)
+        {
+            if (n == 0) return 1;
+            return n * Faktorialis2(n - 1);
         }
     }
 }
