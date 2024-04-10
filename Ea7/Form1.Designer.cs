250a6803fc4d0a5d0c16507154d50258a87f2929
barnabasbaranyistud <barnabas.baranyi2@stud.uni-corvinus.hu>
2024-04-10 15:38:06 +0200

    vmi, baj

--- Ea7/Form1.Designer.cs
+++ Ea7/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -41,21 +44,49 @@
             dataGridView1.Size = new Size(1344, 493);
             dataGridView1.TabIndex = 0;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(1453, 239);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(319, 39);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(1453, 424);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(319, 39);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(1453, 334);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(319, 39);
+            textBox3.TabIndex = 3;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1407, 766);
+            ClientSize = new Size(1830, 766);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
---
+++ Ea7/FormNewCountry.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Ea7
+{
+    partial class FormNewCountry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            buttonOK = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button1.Location = new Point(750, 453);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 0;
+            button1.Text = "Mégse";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonOK
+            // 
+            buttonOK.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonOK.Location = new Point(951, 453);
+            buttonOK.Name = "buttonOK";
+            buttonOK.Size = new Size(150, 46);
+            buttonOK.TabIndex = 1;
+            buttonOK.Text = "OK";
+            buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // FormNewCountry
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1145, 522);
+            Controls.Add(buttonOK);
+            Controls.Add(button1);
+            Name = "FormNewCountry";
+            Text = "FormNewCountry";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button buttonOK;
+    }
+}
